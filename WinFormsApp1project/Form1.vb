c8f8142f2589dbffaf7201f325c33516d9ae7c2e
Visha Hameed <vishahameed111@gmail.com>
2026-02-28 14:24:56 +0500

    Delete WinFormsApp1project directory

--- WinFormsApp1project/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
